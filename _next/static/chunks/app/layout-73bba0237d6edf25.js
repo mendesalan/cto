3f44739702bf7801918870d65c68d195e9ca28c8
Alan <Alan@dev.lan>
2025-05-28 10:08:56 -0300

    Updates

---
+++ _next/static/chunks/app/layout-73bba0237d6edf25.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{347:()=>{},4475:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_a9d16a"}},8719:(e,s,a)=>{Promise.resolve().then(a.t.bind(a,4475,23)),Promise.resolve().then(a.t.bind(a,347,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[725,441,684,358],()=>s(8719)),_N_E=e.O()}]);
--- _next/static/chunks/app/layout-8638b9da638cd9d1.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{347:()=>{},4475:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_d65c78"}},8719:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,4475,23)),Promise.resolve().then(l.t.bind(l,347,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[725,441,684,358],()=>s(8719)),_N_E=e.O()}]);
